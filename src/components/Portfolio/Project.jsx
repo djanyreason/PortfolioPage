cb2b386dfcab106f44622ac4793b3e6ab44845ec
John Anderson <djanyreason@gmail.com>
2024-04-25 16:09:17 -0400

    Adds show/hide functionality for project details

--- src/components/Portfolio/Project.jsx
+++ src/components/Portfolio/Project.jsx
@@ -1,18 +1,42 @@
+import { useState } from 'react';
+
 const Project = ({ proj }) => {
+  const [detail, setDetail] = useState('true');
+  const toggleDetail = () => setDetail(!detail);
+
+  const detailStyle = { marginBottom: 5, display: detail ? 'none' : '' };
+  const projStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  console.log(detail, detailStyle);
+
   return (
-    <div>
-      <p>{proj.name}</p>
-      <a href={proj.url} target='_blank' rel='noreferrer'>
-        {proj.url}
-      </a>
-      <p>{proj.about}</p>
-      <p>
+    <div style={projStyle}>
+      {proj.name}
+      <button
+        onClick={toggleDetail}
+        style={{ marginLeft: 10, marginBottom: 5 }}
+      >
+        {detail ? 'show details' : 'hide details'}
+      </button>
+      <div style={detailStyle}>
+        <a href={proj.url} target='_blank' rel='noreferrer' style={detailStyle}>
+          {proj.url}
+        </a>
+      </div>
+      <div style={detailStyle}>{proj.about}</div>
+      <div style={detailStyle}>
         Keywords:{' '}
         {proj.keywords.reduce(
           (str, kword) => (str === '' ? kword : str + ', ' + kword),
           ''
         )}
-      </p>
+      </div>
     </div>
   );
 };
