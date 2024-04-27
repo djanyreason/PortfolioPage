6fc1308e742fa787fb9a04fa84096b68ef3bd4cf
John Anderson <djanyreason@gmail.com>
2024-04-27 15:32:17 -0400

    Adds createSelector to memoize state

--- src/components/Portfolio/KeywordButtonList.jsx
+++ src/components/Portfolio/KeywordButtonList.jsx
@@ -1,13 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { useSelector } from 'react-redux';
 
 import KeywordButton from './KeywordButton';
 
 const KeywordButtonList = () => {
-  const keywordList = useSelector(({ portfolio, filter }) =>
-    portfolio.keywords.filter(
-      (kword) => kword.toLowerCase().search(filter.text.toLowerCase()) >= 0
-    )
+  const getKwordList = createSelector(
+    [(state) => state.portfolio, (state) => state.filter],
+    (portfolio, filter) =>
+      portfolio.keywords.filter(
+        (kword) => kword.toLowerCase().search(filter.text.toLowerCase()) >= 0
+      )
   );
+
+  const keywordList = useSelector(getKwordList);
 
   if (keywordList.length > 10) return null;
 
--- src/components/Portfolio/ProjectList.jsx
+++ src/components/Portfolio/ProjectList.jsx
@@ -1,17 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { useSelector } from 'react-redux';
 
 import Projects from './Projects';
 
 const ProjectList = () => {
-  const portfolio = useSelector(({ portfolio, filter }) =>
-    portfolio.projects.filter((project) =>
-      project.keywords.find((kword) =>
-        filter.exact
-          ? kword.toLowerCase() === filter.text.toLowerCase()
-          : kword.toLowerCase().search(filter.text.toLowerCase()) >= 0
+  const getMatchingProjects = createSelector(
+    [(state) => state.portfolio, (state) => state.filter],
+    (portfolio, filter) =>
+      portfolio.projects.filter((project) =>
+        project.keywords.find((kword) =>
+          filter.exact
+            ? kword.toLowerCase() === filter.text.toLowerCase()
+            : kword.toLowerCase().search(filter.text.toLowerCase()) >= 0
+        )
       )
-    )
   );
+
+  const portfolio = useSelector(getMatchingProjects);
 
   if (portfolio.length === 0) return <div>No matching projects</div>;
 
