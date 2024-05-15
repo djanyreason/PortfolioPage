d360048b0086565911e1155d0ae684ee793532aa
John Anderson <djanyreason@gmail.com>
2024-05-14 20:22:18 -0500

    Styles footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,11 @@
+import { Typography } from '@mui/material';
+
 const Footer = () => {
-  return <p>Copyright &#169; John F. Anderson 2024</p>;
+  return (
+    <Typography variant='subtitle1' sx={{ my: 1 }}>
+      Copyright &#169; John F. Anderson 2024
+    </Typography>
+  );
 };
 
 export default Footer;
