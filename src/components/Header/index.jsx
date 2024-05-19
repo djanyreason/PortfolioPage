096c5e6e49ce03a7ce270fb996c5f4ee5411369a
John Anderson <djanyreason@gmail.com>
2024-05-19 15:41:13 -0500

    Removes commented out code from Header/index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Paper } from '@mui/material';
@@ -8,38 +7,17 @@ import NarrowLayout from './NarrowLayout';
 import WideLayout from './WideLayout';
 
 const Header = () => {
-  /*  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    window.onresize = () => {
-      setViewportWidth(window.innerWidth);
-    };
-  }, [viewportWidth]);
-*/
-
   const viewport = useSelector(({ display }) => display);
 
   return (
     <Paper elevation={4} sx={{ p: 1.5 }}>
-      {
-        /*viewportWidth >= 400 ? (
-        viewportWidth >= 640 ? (
-          <WideLayout />
-        ) : (
-          <NarrowLayout />
-        )
+      {viewport === 'Wide' ? (
+        <WideLayout />
+      ) : viewport === 'Narrow' ? (
+        <NarrowLayout />
       ) : (
         <MobileLayout />
-      )
-      */
-        viewport === 'Wide' ? (
-          <WideLayout />
-        ) : viewport === 'Narrow' ? (
-          <NarrowLayout />
-        ) : (
-          <MobileLayout />
-        )
-      }
+      )}
     </Paper>
   );
 };
